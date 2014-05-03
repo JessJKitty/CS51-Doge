59f0def69bd31da19a09dac1b38c83f2d3f14700
Jessica Xue <jessicaxu@college.harvard.edu>
2014-05-02 20:20:21 -0400

    ppt

--- Return_max.py
+++ Return_max.py
@@ -4,6 +4,13 @@ from greedy import greedy
 from collapse import collapse
 
 
+##def return_max (function, sentence, word_POS_freqs, transition_probs):
+##    c = function(sentence, word_POS_freqs, transition_probs)
+##    try:
+##        return max([(c[x], x) for x in c])[1]
+##    except: return "miew. errors!"
+
+
 def return_max (function, sentence, word_POS_freqs, transition_probs):
     c = function(sentence, word_POS_freqs, transition_probs)
     return (max(c, key=lambda x: c[x]))
